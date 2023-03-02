4083722e00232219991ac3e553284983f530faee
ata_turkoglu <ata.turkoglu@reengen.com>
2023-03-02 20:32:15 +0300

    add users controller functions

---
+++ controllers/users.js
@@ -0,0 +1,37 @@
+const usersServices = require("../services/users");
+
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  getAll: () => {
+    return usersServices.getAll();
+  },
+  updateUser: (data) => {
+    return usersServices.updateUser(data);
+  },
+  signin: (data) => {
+    if (data.password === data.confirmPassword) {
+      return usersServices.signin(data);
+    } else {
+      return Promise.resolve({ error: "passwords doesnt match each other" });
+    }
+  },
+  login: (data) => {
+    return usersServices
+      .login(data)
+      .then((data) => {
+        if (data.error) {
+          return data.error;
+        } else {
+          let token = jwt.sign(data, process.env.JWT_SECRET_KEY, {
+            expiresIn: 3600,
+          });
+          return { token };
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
