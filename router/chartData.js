d5014a1b9ae8d4b833db9efd7669d61906e3675b
ata_turkoglu <ata.turkoglu@reengen.com>
2023-03-09 00:48:39 +0300

    add getting last total data service

--- router/chartData.js
+++ router/chartData.js
@@ -15,4 +15,18 @@ router.get("/daily", (req, res, next) => {
     });
 });
 
+router.get("/get-last-total", (req, res, next) => {
+  const { facility } = req.query;
+  return chartDataController
+    .getLastDataForAvarage(facility)
+    .then((result) => {
+      res.status(200).send(result);
+      return next();
+    })
+    .catch((exception) => {
+      console.log(exception);
+      return next();
+    });
+});
+
 module.exports = router;
