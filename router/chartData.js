063dc5199fff399997b74af06f0860c3daa0f46d
ata_turkoglu <ata.turkoglu@reengen.com>
2023-03-25 16:48:12 +0300

    add get previous data router for edit

--- router/chartData.js
+++ router/chartData.js
@@ -29,4 +29,18 @@ router.get("/get-last-total", (req, res, next) => {
     });
 });
 
+router.get("/get-previous-total-for-edit", (req, res, next) => {
+  const { facility } = req.query;
+  return chartDataController
+    .getPreviousDataForEdit(facility)
+    .then((result) => {
+      res.status(200).send(result);
+      return next();
+    })
+    .catch((exception) => {
+      console.log(exception);
+      return next();
+    });
+});
+
 module.exports = router;
