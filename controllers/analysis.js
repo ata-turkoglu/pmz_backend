fa704b14b8c54c482b5b7d50429ad6386bf6c7c4
ata_turkoglu <ata.turkoglu@reengen.com>
2023-07-04 23:19:30 +0300

    add analysis controllers and services

--- controllers/analysis.js
+++ controllers/analysis.js
@@ -1,14 +1,62 @@
 const analysisServices = require("../services/analysis.js");
 module.exports = {
+    save3060Mesh: (data) => {
+        let obj = {
+            bigbag_no: parseInt(data.bigbagNo),
+            notes: data.notes,
+        };
+
+        return analysisServices.save3060Mesh(obj);
+    },
+
+    get80Mesh: () => {
+        return analysisServices.get80Mesh().then((res) => {
+            res.forEach((item) => {
+                item.bigbagNo = item.bigbag_no;
+                delete item.bigbag_no;
+            });
+            return res;
+        });
+    },
+
     save80Mesh: (data) => {
         let obj = {
             bigbag_no: parseInt(data.bigbagNo),
-            p400: parseInt(data.p400),
-            m212: parseInt(data.m212),
-            m160: parseInt(data.m160),
+            p400: data.p400,
+            m212: data.m212,
+            m160: data.m160,
             notes: data.notes,
         };
 
         return analysisServices.save80Mesh(obj);
+    },
+
+    update80Mesh: (data) => {
+        let obj = {
+            id: data.id,
+            bigbag_no: parseInt(data.bigbagNo),
+            p400: data.p400,
+            m212: data.m212,
+            m160: data.m160,
+            notes: data.notes,
+        };
+
+        return analysisServices.update80Mesh(obj);
+    },
+
+    delete80Mesh: (id) => {
+        return analysisServices.delete80Mesh(id);
+    },
+
+    save180Mesh: (data) => {
+        let obj = {
+            bigbag_no: parseInt(data.bigbagNo),
+            p400: parseInt(data.p212),
+            m212: parseInt(data.p160),
+            m160: parseInt(data.m90),
+            notes: data.notes,
+        };
+
+        return analysisServices.save180Mesh(obj);
     },
 };
--- services/analysis.js
+++ services/analysis.js
@@ -1,8 +1,78 @@
 const db = require("../db");
 
 module.exports = {
+    save3060Mesh: (data) => {
+        return db("analysis3060mesh")
+            .insert(data)
+            .returning("id")
+            .then((result) => {
+                return result;
+            })
+            .catch((error) => {
+                console.log(error);
+                return { error };
+            });
+    },
+
+    get80Mesh: () => {
+        return db("analysis80mesh")
+            .select()
+            .then((result) => {
+                return result;
+            })
+            .catch((error) => {
+                console.log(error);
+                return { error };
+            });
+    },
+
     save80Mesh: (data) => {
         return db("analysis80mesh")
+            .insert(data)
+            .returning("id")
+            .then((result) => {
+                return result;
+            })
+            .catch((error) => {
+                console.log(error);
+                return { error };
+            });
+    },
+
+    update80Mesh: (data) => {
+        return db("analysis80mesh")
+            .update({
+                bigbag_no: data.bigbag_no,
+                p400: data.p400,
+                m212: data.m212,
+                m160: data.m160,
+                notes: data.notes,
+            })
+            .where({ id: data.id })
+            .then((result) => {
+                return result;
+            })
+            .catch((error) => {
+                console.log(error);
+                return { error };
+            });
+    },
+
+    delete80Mesh: (id) => {
+        return db("analysis80mesh")
+            .del()
+            .where({ id })
+            .then((result) => {
+                return result;
+            })
+            .catch((error) => {
+                console.log(error);
+                return { error };
+            });
+    },
+
+    save180Mesh: (data) => {
+        return db("analysis180mesh")
             .insert(data)
             .returning("id")
             .then((result) => {
