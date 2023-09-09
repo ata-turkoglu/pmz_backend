080a6a7a41a1660943a45dc19c3c1c509583a558
ata_turkoglu <ata.turkoglu@reengen.com>
2023-09-09 11:35:58 +0300

    add new packaging feature on stocktaking service file

--- services/quartz/product/stocktaking.js
+++ services/quartz/product/stocktaking.js
@@ -1,4 +1,17 @@
 const db = require("../../../db");
+const moment = require("moment");
+
+const setLastDate = (result) => {
+    return new Promise((resolve) => {
+        let lastDate;
+        if (result == []) {
+            lastDate = moment("01-09-2023", "DD-MM-YYYY").format(); // 2023-09-01T00:00:00+03:00
+        } else {
+            lastDate = moment(result[0]?.workday).format();
+        }
+        resolve(lastDate);
+    });
+};
 
 module.exports = {
     addStocktakingData: (data) => {
@@ -13,13 +26,38 @@ module.exports = {
                 return { error };
             });
     },
-    getLastDate: () => {
+    addPackagingData: (data) => {
+        return db("quartz_packaging_usage")
+            .insert(...data)
+            .returning("id")
+            .then((result) => {
+                return result;
+            })
+            .catch((error) => {
+                console.log(error);
+                return { error };
+            });
+    },
+    getLastDateOfProducing: () => {
         return db("quartz_product_stocktaking")
             .select("workday")
             .orderBy("workday", "desc")
             .limit(1)
-            .then((result) => {
-                return result;
+            .then(async (result) => {
+                return await setLastDate(result);
+            })
+            .catch((error) => {
+                console.log(error);
+                return { error };
+            });
+    },
+    getLastDateOfPackaging: () => {
+        return db("quartz_packaging_usage")
+            .select("workday")
+            .orderBy("workday", "desc")
+            .limit(1)
+            .then(async (result) => {
+                return await setLastDate(result);
             })
             .catch((error) => {
                 console.log(error);
