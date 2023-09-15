6d8cc89b003e8af08313f34dcad73ea073413074
ata_turkoglu <ata.turkoglu@reengen.com>
2023-09-15 16:35:52 +0300

    set response for cron

--- router/utils.js
+++ router/utils.js
@@ -4,6 +4,8 @@ const stocktakingController = require("../controllers/quartz/product/stocktaking
 
 router.get("/checkMails", (req, res, next) => {
     logger.info("checkMails");
+    res.status(200).send(true);
+    return next();
     //stocktakingController.addStocktakingData()
 });
 
