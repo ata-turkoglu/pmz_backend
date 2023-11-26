2024c62f94af548b8d49eb616162ad038b2b55df
ata_turkoglu <ata.turkoglu@reengen.com>
2023-11-26 20:20:29 +0300

    add consumables controller and service

--- controllers/quartz/rawMaterials/consumables.js
+++ controllers/quartz/rawMaterials/consumables.js
@@ -1,4 +1,5 @@
 const consumabalesService = require("../../../services/quartz/rawMaterials/consumables");
+const moment = require("moment");
 
 module.exports = {
     getLastDate: (data) => {
@@ -6,5 +7,11 @@ module.exports = {
     },
     addConsumable: (data) => {
         return consumabalesService.addConsumable(data);
+    },
+    getPurchasedConsumablesByYear: (year) => {
+        const start = moment([year]).startOf("year").format("YYYY-MM-DD");
+        const end = moment([year]).endOf("year").format("YYYY-MM-DD");
+
+        return consumabalesService.getPurchasedConsumablesByYear([start, end]);
     },
 };
--- services/quartz/rawMaterials/consumables.js
+++ services/quartz/rawMaterials/consumables.js
@@ -28,4 +28,18 @@ module.exports = {
                 return { error };
             });
     },
+    getPurchasedConsumablesByYear: (dateRange) => {
+        return db("consumables")
+            .select()
+            .whereBetween("invoice_date", [dateRange[0], dateRange[1]])
+            .andWhere("facility", 5)
+            .orderBy("invoice_date", "desc")
+            .then((result) => {
+                return result;
+            })
+            .catch((error) => {
+                console.log(error);
+                return { error };
+            });
+    },
 };
